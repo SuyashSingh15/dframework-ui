b6c40927a082ae0c36193d4fcf4cc79a9fb8fbca
SuyashSingh <suyash@spraxa.com>
2024-03-04 19:35:31 +0530

    fixes for select and local

--- dist/components/Form/fields/select.js
+++ dist/components/Form/fields/select.js
@@ -34,85 +34,46 @@ const Field = _ref => {
   if (column.multiSelect) {
     if (!inputValue || inputValue.length === 0) {
       inputValue = [];
-    } else {
-      if (!Array.isArray(inputValue)) {
-        inputValue = inputValue.split(",").map(e => parseInt(e));
-      }
+    } else if (!Array.isArray(inputValue)) {
+      inputValue = inputValue.split(",").map(e => parseInt(e));
     }
   }
-  if (column.modifiedLabel) {
-    if (field === "ActualRoomId" && formik.values.HospitalId) {
-      const selectedHospitalId = formik.values.HospitalId;
-      options = options.filter(option => option.HospitalId === selectedHospitalId);
-    }
-    return /*#__PURE__*/_react.default.createElement(_material.FormControl, {
-      sx: {
-        marginRight: ".9rem"
-      },
-      fullWidth: true,
-      key: field
-    }, /*#__PURE__*/_react.default.createElement(_material.InputLabel, {
-      sx: {
-        margin: "0.9rem 2rem 1.5rem 0rem",
-        position: "absolute",
-        zIndex: "1",
-        transform: "translate(14px, -9px) scale(0.75)",
-        color: formik.touched[field] && formik.errors[field] ? "#f44336" : "inherit"
-      }
-    }, column.label), /*#__PURE__*/_react.default.createElement(_material.Select, _extends({
-      error: formik.touched[field] && formik.errors[field],
-      IconComponent: _KeyboardArrowDown.default
-    }, otherProps, {
-      name: field,
-      placeholder: "Select",
-      multiple: column.multiSelect === true,
-      readOnly: column.readOnly === true,
-      value: inputValue,
-      renderValue: selected => {
-        if (Array.isArray(selected)) {
-          return selected.map(value => {
-            const option = options.find(option => option.value === value);
-            return option ? option.label : "Select";
-          }).join(", ");
-        } else {
-          const selectedOption = options.find(option => option.value === parseInt(selected));
-          return selectedOption ? selectedOption.label : "Select";
-        }
-      },
-      sx: {
-        // width: "337px",
-        backgroundColor: " #4F5883 !important",
-        paddingTop: ".9rem",
-        height: "3.313rem"
-      },
-      onChange: formik.handleChange,
-      onBlur: formik.handleBlur
-    }), Array.isArray(options) && options.map(option => /*#__PURE__*/_react.default.createElement(_material.MenuItem, {
-      key: option.value,
-      value: option.value
-    }, option.label))), /*#__PURE__*/_react.default.createElement(_material.FormHelperText, {
-      style: {
-        color: "#f44336"
-      }
-    }, formik.touched[field] && formik.errors[field]));
-  } else {
-    return /*#__PURE__*/_react.default.createElement(_material.FormControl, {
-      fullWidth: true,
-      key: field,
-      variant: "standard"
-    }, /*#__PURE__*/_react.default.createElement(_material.InputLabel, null, fieldLabel), /*#__PURE__*/_react.default.createElement(_material.Select, _extends({
-      IconComponent: _KeyboardArrowDown.default
-    }, otherProps, {
-      name: field,
-      multiple: column.multiSelect === true,
-      readOnly: column.readOnly === true,
-      value: inputValue,
-      onChange: formik.handleChange,
-      onBlur: formik.handleBlur
-    }), Array.isArray(options) && options.map(option => /*#__PURE__*/_react.default.createElement(_material.MenuItem, {
-      key: option.value,
-      value: option.value
-    }, option.label))), /*#__PURE__*/_react.default.createElement(_material.FormHelperText, null, formik.touched[field] && formik.errors[field]));
+  if (field === "ActualRoomId" && formik.values.HospitalId) {
+    const selectedHospitalId = formik.values.HospitalId;
+    options = options.filter(option => option.HospitalId === selectedHospitalId);
   }
+  return /*#__PURE__*/_react.default.createElement(_material.FormControl, {
+    fullWidth: true,
+    key: field,
+    variant: "standard"
+  }, /*#__PURE__*/_react.default.createElement(_material.InputLabel, {
+    error: formik.touched[field] && formik.errors[field]
+  }, fieldLabel || column.label), /*#__PURE__*/_react.default.createElement(_material.Select, _extends({
+    IconComponent: _KeyboardArrowDown.default
+  }, otherProps, {
+    error: formik.touched[field] && formik.errors[field],
+    name: field,
+    multiple: column.multiSelect === true,
+    readOnly: column.readOnly === true,
+    value: inputValue,
+    renderValue: selected => {
+      if (Array.isArray(selected)) {
+        return selected.map(value => {
+          const option = options.find(option => option.value === value);
+          return option ? option.label : "Select";
+        }).join(", ");
+      } else {
+        const selectedOption = options.find(option => option.value === parseInt(selected));
+        return selectedOption ? selectedOption.label : "Select";
+      }
+    },
+    onChange: formik.handleChange,
+    onBlur: formik.handleBlur
+  }), Array.isArray(options) && options.map(option => /*#__PURE__*/_react.default.createElement(_material.MenuItem, {
+    key: option.value,
+    value: option.value
+  }, option.label))), formik.touched[field] && formik.errors[field] && /*#__PURE__*/_react.default.createElement(_material.FormHelperText, {
+    error: true
+  }, formik.errors[field]));
 };
 var _default = exports.default = Field;
--- src/lib/components/Form/fields/select.js
+++ src/lib/components/Form/fields/select.js
@@ -29,107 +29,61 @@ const Field = ({
     if (column.multiSelect) {
         if (!inputValue || inputValue.length === 0) {
             inputValue = [];
-        } else {
-            if (!Array.isArray(inputValue)) {
-                inputValue = inputValue.split(",").map((e) => parseInt(e));
+        } else if (!Array.isArray(inputValue)) {
+            inputValue = inputValue.split(",").map((e) => parseInt(e));
+        }
+    }
+    if (field === "ActualRoomId" && formik.values.HospitalId) {
+        const selectedHospitalId = formik.values.HospitalId;
+        options = options.filter(
+            (option) => option.HospitalId === selectedHospitalId
+        );
+    }
+    return (
+        <FormControl fullWidth key={field} variant="standard">
+            <InputLabel error={formik.touched[field] && formik.errors[field]}>{fieldLabel || column.label}</InputLabel>
+            <Select
+                IconComponent={KeyboardArrowDownIcon}
+                {...otherProps}
+                error={formik.touched[field] && formik.errors[field]}
+                name={field}
+                multiple={column.multiSelect === true}
+                readOnly={column.readOnly === true}
+                value={inputValue}
+                renderValue={(selected) => {
+                    if (Array.isArray(selected)) {
+                        return selected
+                            .map((value) => {
+                                const option = options.find(
+                                    (option) => option.value === value
+                                );
+                                return option ? option.label : "Select";
+                            })
+                            .join(", ");
+                    } else {
+                        const selectedOption = options.find(
+                            (option) => option.value === parseInt(selected)
+                        );
+                        return selectedOption ? selectedOption.label : "Select";
+                    }
+                }}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+            >
+                {Array.isArray(options) &&
+                    options.map((option) => (
+                        <MenuItem key={option.value} value={option.value}>
+                            {option.label}
+                        </MenuItem>
+                    ))}
+            </Select>
+            {formik.touched[field] && formik.errors[field] &&
+                (<FormHelperText error={true}>
+                    {formik.errors[field]}
+                </FormHelperText>)
             }
-        }
-    }
-
-    if (column.modifiedLabel) {
-        if (field === "ActualRoomId" && formik.values.HospitalId) {
-            const selectedHospitalId = formik.values.HospitalId;
-            options = options.filter(
-                (option) => option.HospitalId === selectedHospitalId
-            );
-        }
-        return (
-            <FormControl sx={{ marginRight: ".9rem" }} fullWidth key={field}>
-                <InputLabel
-                    sx={{
-                        margin: "0.9rem 2rem 1.5rem 0rem",
-                        position: "absolute",
-                        zIndex: "1",
-                        transform: "translate(14px, -9px) scale(0.75)",
-                        color: formik.touched[field] && formik.errors[field] ? "#f44336" : "inherit"
-                    }}
-                >
-                    {column.label}
-                </InputLabel>
-                <Select
-                    error={formik.touched[field] && formik.errors[field]}
-                    IconComponent={KeyboardArrowDownIcon}
-                    {...otherProps}
-                    name={field}
-                    placeholder="Select"
-                    multiple={column.multiSelect === true}
-                    readOnly={column.readOnly === true}
-                    value={inputValue}
-                    renderValue={(selected) => {
-                        if (Array.isArray(selected)) {
-                            return selected
-                                .map((value) => {
-                                    const option = options.find(
-                                        (option) => option.value === value
-                                    );
-                                    return option ? option.label : "Select";
-                                })
-                                .join(", ");
-                        } else {
-                            const selectedOption = options.find(
-                                (option) => option.value === parseInt(selected)
-                            );
-                            return selectedOption ? selectedOption.label : "Select";
-                        }
-                    }}
-                    sx={{
-                        // width: "337px",
-                        backgroundColor: " #4F5883 !important",
-                        paddingTop: ".9rem",
-                        height: "3.313rem",
-                    }}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                >
-                    {Array.isArray(options) &&
-                        options.map((option) => (
-                            <MenuItem key={option.value} value={option.value}>
-                                {option.label}
-                            </MenuItem>
-                        ))}
-                </Select>
-                <FormHelperText style={{ color: "#f44336" }}>
-                    {formik.touched[field] && formik.errors[field]}
-                </FormHelperText>
-            </FormControl>
-        );
-    } else {
-        return (
-            <FormControl fullWidth key={field} variant="standard">
-                <InputLabel>{fieldLabel}</InputLabel>
-                <Select
-                    IconComponent={KeyboardArrowDownIcon}
-                    {...otherProps}
-                    name={field}
-                    multiple={column.multiSelect === true}
-                    readOnly={column.readOnly === true}
-                    value={inputValue}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                >
-                    {Array.isArray(options) &&
-                        options.map((option) => (
-                            <MenuItem key={option.value} value={option.value}>
-                                {option.label}
-                            </MenuItem>
-                        ))}
-                </Select>
-                <FormHelperText>
-                    {formik.touched[field] && formik.errors[field]}
-                </FormHelperText>
-            </FormControl>
-        );
-    }
+        </FormControl>
+    );
 };
 
 export default Field;
